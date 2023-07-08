9ff60ac7d2fad2ec89adf27b344e9e8ec3bbf74e
xiu <gao.yh1991@gmail.com>
2023-07-08 10:21:27 +0800

    Update main.js

--- micro-apps/micro-vue/src/main.js
+++ micro-apps/micro-vue/src/main.js
@@ -6,15 +6,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MHome from './components/MHome.vue'
 import MAbout from './components/MAbout.vue'
 
-
-// 2. 定义一些路由
-// 每个路由都需要映射到一个组件。
-// 我们后面再讨论嵌套路由。
 const routes = [
     { path: '/', component: MHome },
     { path: '/about', component: MAbout },
 ]
-
 
 let instance = null;
 let router = null;
